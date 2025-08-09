cbc3cb3376f3486f95c1f5df1aa08bfda3c9df86
dean <deanfernandes18@gmail.com>
2025-08-09 12:25:34 +0100

    Add db service; Replace in memory with db in users controller

--- backend/api-service/src/controllers/users-controller.ts
+++ backend/api-service/src/controllers/users-controller.ts
@@ -1,57 +1,31 @@
 import type { RequestHandler } from "express";
 import type User from "../models/user";
 import { hashPassword } from "../services/password-service";
-
-const users: User[] = [];
+import * as databaseService from "../services/database-service";
 
 export const createUser: RequestHandler = async (req, res) => {
   const user = req.body as User;
   user.password = await hashPassword(user.password);
-  user.id = users.length + 1;
-  users.push(user);
-  res.status(201).json(user);
+  res.status(201).json(await databaseService.createUser(user));
 };
 
 export const getUsers: RequestHandler = async (req, res) => {
-  res.json(users);
+  res.json(await databaseService.getUsers());
 };
 
 export const getUserById: RequestHandler = async (req, res) => {
-  const id = Number(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ message: "Invalid user id" });
-  }
-  const user = users.find((u) => u.id === id);
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-  res.json(user);
+  res.json(await databaseService.getUserById(Number(req.params.id)));
 };
 
 export const updateUser: RequestHandler = async (req, res) => {
-  const id = Number(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ message: "Invalid user id" });
-  }
-  const index = users.findIndex((u) => u.id === id);
-  if (index === -1) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  const updatedUser = { ...users[index], ...req.body };
-  users[index] = updatedUser;
-  res.json(updatedUser);
+  const user = req.body as User;
+  user.id = Number(req.params.id);
+  user.password = await hashPassword(user.password);
+  await databaseService.updateUser(user);
+  res.status(200).send();
 };
 
 export const deleteUser: RequestHandler = async (req, res) => {
-  const id = Number(req.params.id);
-  if (isNaN(id)) {
-    return res.status(400).json({ message: "Invalid user id" });
-  }
-  const index = users.findIndex((u) => u.id === id);
-  if (index === -1) {
-    return res.status(404).json({ message: "User not found" });
-  }
-  users.splice(index, 1);
+  await databaseService.deleteUser(Number(req.params.id));
   res.status(204).send();
 };
