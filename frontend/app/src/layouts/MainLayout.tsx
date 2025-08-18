5e4f652f7108dc86a3cef9f8f1d0af31d1545eef
dean <deanfernandes18@gmail.com>
2025-08-18 14:47:58 +0100

    Add chat pkg; Add chat componenet to main; Update nginx reverse proxy for chat service; Use official frontend docker image name in docker run shell script

--- frontend/app/src/layouts/MainLayout.tsx
+++ frontend/app/src/layouts/MainLayout.tsx
@@ -1,15 +1,24 @@
 import { Outlet } from "react-router";
 import ProductsNavBar from "../components/products/ProductsNavBar";
+import Chat from "@deanfernandes/react-chat-ws-client";
 
 interface MainLayoutProps {
   token: string | null;
 }
 
 const MainLayout = ({ token }: MainLayoutProps) => {
+  const wsProtocol = window.location.protocol === "https:" ? "wss" : "ws";
+  const wsUrl = `${wsProtocol}://${window.location.host}/chat`;
+
   return (
     <main className="flex-grow flex flex-col bg-white dark:bg-gray-700 text-gray-700 dark:text-white">
       {token && <ProductsNavBar />}
       <Outlet />
+      {token && (
+        <div className="fixed z-50 bottom-32 right-10 hidden md:block">
+          <Chat serverAddress={wsUrl} token={token} />
+        </div>
+      )}
     </main>
   );
 };
--- frontend/docker_run.sh
+++ frontend/docker_run.sh
@@ -4,4 +4,4 @@ docker run -p 80:80 -p 443:443 \
   --rm \
   -v "$(pwd)/nginx/default.conf:/etc/nginx/conf.d/default.conf:ro" \
   -v "$(pwd)/nginx/ssl:/etc/nginx/ssl:ro" \
-  frontend:latest
+  ghcr.io/deanfernandes/frontend:latest
